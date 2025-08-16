e79f0aa16c0ab875c8d724f959096b69c2939e8e
mamemamema <st.msnr.wk@gmail.com>
2025-08-16 23:46:11 +0000

    [Ymobile]Site Change

--- automation_ymobile.py
+++ automation_ymobile.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from playwright.sync_api import expect
 
 from automation_base import AutomationBase
@@ -19,25 +21,31 @@ class AutomationYmobile(AutomationBase):
         self.page.keyboard.press("Tab")
         self.page.get_by_role("button", name="ログインする").click()
         self.page.wait_for_load_state()
-        self.verify_ident()
+        self.verify_ident("softbank")
 
-    def verify_ident(self):
-        if self.wait_for_page("本人確認 | ワイモバイル"):
+    def verify_ident(self, displayType: Literal["softbank", "ymobile"]):
+        title = "本人確認 | ワイモバイル"
+        if displayType == "softbank":
+            title = "本人確認 | My SoftBank | ソフトバンク"
+
+        if self.wait_for_page(title):
             auth_code = input("Input Auth Code:")
             self.page.locator('input[name="password"]').fill(auth_code)
             self.page.wait_for_timeout(1000)
             self.page.keyboard.press("Tab")
             self.page.get_by_role("button", name="本人確認する").click()
             self.page.wait_for_load_state()
+            self.page.wait_for_timeout(2000)
     
     def download_invoice(self):
         self.page.get_by_role("link", name="料金案内").locator("..").click()
         self.page.wait_for_load_state()
+        self.page.wait_for_timeout(2000)
         self.page.get_by_role("link", name="書面発行").click()
         self.page.wait_for_load_state()
         self.page.get_by_text("自分で印刷する（無料）").first.click()
         self.page.wait_for_load_state()
-        self.verify_ident()
+        self.verify_ident("ymobile")
         self.page.get_by_role("link", name="一括印刷用PDFファイル").click()
         # NOTE: イベント待ちのままタイムアウトになる。
         # self.page.wait_for_load_state()
